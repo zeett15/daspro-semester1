9f1b532bddf19aeb404e5740028ee7dfcc96aac0
Qruzeeta Prasna <qruzeetaprasna15@gmail.com>
2024-09-04 14:41:45 +0700

    jobsheet 1

---
+++ jobsheet1/MyFristJava.java
@@ -0,0 +1,11 @@
+package SorceCode.java;
+
+/**
+ * MyFristJava
+ */
+public class MyFristJava {
+
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Qruzeeta Prasna");
+    }
+}
---
+++ jobsheet1/Tugas22.java
@@ -0,0 +1,16 @@
+package SorceCode.java;
+
+/**
+ * Tugas22
+ */
+public class Tugas22 {
+
+    public static void main(String[] args) {
+        System.out.println("-------------------------------");
+        System.out.println("BIODATA MAHASISWA BARU POLINEMA");
+        System.out.println("-------------------------------");
+        System.out.println( "Nama\t: Qruzeeta Prasna");
+        System.out.println("NIM\t: 244107020084");
+        System.out.println("Alamat\t: Jl. Natai Arahan no. 14");
+    }
+}
