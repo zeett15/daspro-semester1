445aeabdec3299bc3414f1787e152c26529b2641
Qruzeeta Prasna <qruzeetaprasna15@gmail.com>
2024-09-24 00:12:22 +0700

    menambahkan jobsheet baru

---
+++ jobsheet2/MyFristJavaGithub22.java
@@ -0,0 +1,10 @@
+/**
+ * MyFristJavaGithub22
+ */
+public class MyFristJavaGithub22 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello form Github");
+        System.out.println();
+    }
+}
