fefd5d0d09547e933bfc361554d473a3cbb68e44
Qruzeeta Prasna <qruzeetaprasna15@gmail.com>
2024-10-14 16:42:13 +0700

    menambahkan fitur lulus dan tidak lulus pada file siakadFor

--- jobsheet7/SiakadFor22.java
+++ jobsheet7/SiakadFor22.java
@@ -9,6 +9,7 @@ public class SiakadFor22 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus =1, tidakLulus = 1;
 
         for (int i =1;i <= 10; i++){
             System.out.print("Masukan  nilai mahasiswa ke-" + i + ": ");
@@ -20,8 +21,16 @@ public class SiakadFor22 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            }else{
+                tidakLulus++;
+            }
         }
+        System.out.println("===============================");
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("lulus: " + lulus + " orang");
+        System.out.println("tidak lulus: " + tidakLulus + " orang");
     }
 }
